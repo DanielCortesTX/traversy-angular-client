7d3cdf4701a76f01bbb1056873ad259bd20f0b3a
Daniel Cortes <d5t5c5@yahoo.com>
2019-03-10 20:26:07 -0500

    feat: Added client service and angularfire2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD3I3bjEBBzNmjA4K_D11V1rtHVdMLV78M",
+    authDomain: "clientpanelprod-d790a.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-d790a.firebaseio.com",
+    projectId: "clientpanelprod-d790a",
+    storageBucket: "clientpanelprod-d790a.appspot.com",
+    messagingSenderId: "1081987146107"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD3I3bjEBBzNmjA4K_D11V1rtHVdMLV78M",
+    authDomain: "clientpanelprod-d790a.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-d790a.firebaseio.com",
+    projectId: "clientpanelprod-d790a",
+    storageBucket: "clientpanelprod-d790a.appspot.com",
+    messagingSenderId: "1081987146107"
+  }
 };
 
 /*
